cca8112582f3233670b9670e9f13d4f77201460b
danul <danul78969@gmail.com>
2025-04-05 15:13:25 +0300

    added init

--- src/__main__.py
+++
@@ -1,7 +0,0 @@
-from src.context.AnalisisContext import AnalisContext
-from src.AnalisisManager import AnalisisManager
-
-if __name__ == "__main__":
-    analis_context = AnalisContext(ru_city_name="Санкт-Петербург")
-    analis_manager = AnalisisManager(analis_context)
-    analis_manager.process()
